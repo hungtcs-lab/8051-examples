a24476a07038f11cb61c458d1709e58c1c3503ef
鸿则 <hungtcs@163.com>
2018-05-30 17:13:30 +0800

    更新初始化方式

--- lcd1602/src/lcd1602.c
+++ lcd1602/src/lcd1602.c
@@ -29,16 +29,36 @@ void lcd1602_clear()
 
 void lcd1602_init()
 {
-  lcd1602_write(0x38, LCD1602_COMMAND);
+  // lcd1602_write(0x38, LCD1602_COMMAND);
+  // delay(5);
+  // lcd1602_write(0x38, LCD1602_COMMAND);
+  // delay(5);
+  // lcd1602_write(0x38, LCD1602_COMMAND);
+  // delay(5);
+  // lcd1602_write(0x38, LCD1602_COMMAND);
+  // lcd1602_write(0x08, LCD1602_COMMAND);
+  // lcd1602_write(0x01, LCD1602_COMMAND);
+  // lcd1602_write(0x06, LCD1602_COMMAND);
+  // delay(5);
+  // lcd1602_write(0x0c, LCD1602_COMMAND);
+
+  // 清屏
   delay(5);
-  lcd1602_write(0x38, LCD1602_COMMAND);
-  delay(5);
-  lcd1602_write(0x38, LCD1602_COMMAND);
-  delay(5);
-  lcd1602_write(0x38, LCD1602_COMMAND);
-  lcd1602_write(0x08, LCD1602_COMMAND);
   lcd1602_write(0x01, LCD1602_COMMAND);
+  delay(5);
+  // 光标复位
+  lcd1602_write(0x03, LCD1602_COMMAND);
+  delay(5);
+  // 光标右移，不移屏
   lcd1602_write(0x06, LCD1602_COMMAND);
   delay(5);
-  lcd1602_write(0x0c, LCD1602_COMMAND);
+  // 开显示，开光标，光标闪烁
+  lcd1602_write(0x0e, LCD1602_COMMAND);
+  delay(1);
+  // 光标自动右移
+  lcd1602_write(0x14, LCD1602_COMMAND);
+  delay(1);
+  // 8位并行，两行显示，5*7字符大小
+  lcd1602_write(0x38, LCD1602_COMMAND);
+  delay(1);
 }
