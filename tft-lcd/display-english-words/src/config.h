2e89956a93935c547bee1d6d4d27c7286ff1e421
鸿则 <hungtcs@163.com>
2019-04-05 17:30:48 +0800

    feat: 添加TFT LCD液晶显示英文例子

---
+++ tft-lcd/display-english-words/src/config.h
@@ -0,0 +1,14 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// spi port
+#define SPI_CS     P1_3
+#define SPI_SCK    P1_7
+#define SPI_SDA    P1_5
+
+// lcd port
+#define LCD_RS     P1_2
+#define LCD_LED    P3_2
+#define LCD_RESET  P3_3
+
+#endif
