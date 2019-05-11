feb381027389b90d8b30b12717808db5b50118b0
hungtcs <hungtcs@163.com>
2019-05-11 21:05:11 +0800

    fix: 更新Makefile

--- Modules/NRF24L01/src/config.h
+++ Modules/NRF24L01/src/config.h
@@ -4,16 +4,16 @@
 #include <mcs51/8051.h>
 
 // spi pins
-#define SPI_CS          P1_1
-#define SPI_SCK         P1_2
-#define SPI_MOSI        P1_3
-#define SPI_MISO        P1_4
+#define SPI_CS          P2_4
+#define SPI_SCK         P2_3
+#define SPI_MOSI        P2_2
+#define SPI_MISO        P2_1
 
 // nRF24L01 pins
-#define NRF24_CE        P1_0
-#define NRF24_IRQ       P1_5
+#define NRF24_CE        P2_5
+#define NRF24_IRQ       P2_0
 
-#define NRF24_RECEIVER
-// #define NRF24_TRANSMITTER
+// #define NRF24_RECEIVER
+#define NRF24_TRANSMITTER
 
 #endif
